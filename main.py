75de1813feffc34091bfa8bfadd261e9dc4a60d7
sailll choi <sailll@sina.cn>
2016-02-26 18:41:24 +0800

    faceless
    how are you

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 a='whoareyou~'
 b='you'
 print a.find(b,7)
@@ -5,3 +6,10 @@ print a.upper()
 print min(a)
 print max(a)
 
+=======
+str='a a a a';
+s1='a'
+s2='b'
+str=str.replace(s1,s2);
+print str
+>>>>>>> Stashed changes
