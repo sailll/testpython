d12e938478f35e940230f09e3003937cabcbc4c2
sailll choi <sailll@sina.cn>
2016-02-29 10:07:16 +0800

    new change

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-<<<<<<< Updated upstream
-a='whoareyou~'
-b='you'
-print a.find(b,7)
-print a.upper()
-print min(a)
-print max(a)
-
-=======
-str='a a a a';
-s1='a'
-s2='b'
-str=str.replace(s1,s2);
-print str
->>>>>>> Stashed changes
+list =['physics','math',34]
+list[1]='chemistry'
+print list[2]
+list+='chinese'
+print len(list)
